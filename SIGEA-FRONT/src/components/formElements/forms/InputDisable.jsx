7878b4d33445154d1551c0bff98c83ea0f3bccbb
Miguel Munar <dev.mianm@gmail.com>
2026-07-30 13:23:03 -0500

    fixeo de problema de dependencias

--- SIGEA-FRONT/src/components/formElements/forms/InputDisable.jsx
+++ SIGEA-FRONT/src/components/formElements/forms/InputDisable.jsx
@@ -2,7 +2,7 @@ export default function InputDisable({ textLabel, dataText }){
     return(
         <label className="flex flex-col flex-1 bg-[#c4b5b5] p-4 pr-6 rounded-[7px] font-bold">
             {textLabel}
-            <div className="text-[rgba(0,0,0,0.512)] bg-[#ffffff61] px-[9px] py-3 mt-[7px] rounded-[15px]">
+            <div className="text-[rgba(0,0,0,0.512)] bg-[#ffffff61] px-2.25 py-3 mt-1.75 rounded-[15px]">
                 {dataText}
             </div>
         </label>
